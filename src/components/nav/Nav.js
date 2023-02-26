e3b206ce00bd19be54bc7a44c672179fdc64ad91
SaraAliMohammed <sara.ali.mohammed213@gmail.com>
2023-02-26 11:02:03 +0200

    Main Start

---
+++ src/components/nav/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Nav.css'
+
+const Nav = () => {
+  return (
+    <nav className="navbar">
+      <ul className="nav">
+        <li><a>Home</a></li>
+        <li><a>Conact</a></li>
+        <li><a>About</a></li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Nav
